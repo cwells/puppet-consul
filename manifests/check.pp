b8adec5178b87fa550d37f89dff927de8a0809eb
Cliff Wells <cwells@focusvision.com>
2018-12-11 11:15:02 -0800

    remove pointless and broken json beautification functions

--- manifests/check.pp
+++ manifests/check.pp
@@ -96,7 +96,7 @@ define consul::check(
     owner   => $consul::user_real,
     group   => $consul::group_real,
     mode    => $consul::config_mode,
-    content => consul_sorted_json($check_hash, $consul::pretty_config, $consul::pretty_config_indent),
+    content => to_json_pretty($check_hash)
   } ~> Class['consul::reload_service']
 
 }
